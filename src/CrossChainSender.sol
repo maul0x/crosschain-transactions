4f901b6f74f1f417dd2bb86b3da85823a0b88863
maul0x <501staudits@gmail.com>
2025-10-01 07:13:19 -0700

    feat: Add constructor

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -4,4 +4,13 @@ pragma solidity ^0.8.13;
 import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
 import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
-contract CrossChainSender {}
+contract CrossChainSender {
+
+        constructor(
+        address _wormholeRelayer,
+        address _tokenBridge,
+        address _wormhole
+    )
+        TokenBase(_wormholeRelayer, _tokenBridge, _wormhole)
+    { }
+}
