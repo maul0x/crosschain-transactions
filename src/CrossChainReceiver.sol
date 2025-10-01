680619b55495d59632635074722a12b45a2eccba
maul0x <501staudits@gmail.com>
2025-10-01 07:00:21 -0700

    feat: Add CrossChainReceiver Contract Title Section Natspec Comments

--- src/CrossChainReceiver.sol
+++ src/CrossChainReceiver.sol
@@ -4,4 +4,9 @@ pragma solidity ^0.8.13;
 import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
 import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
+/**
+ * @title CrossChainReceiver.
+ * @notice A contract to handle receiving of tokens cross chain using wormhole cross chain token transfer.
+ * @notice This contract is deployed on multiple chains to support multiple target chains.
+ */
 contract CrossChainReceiver {}
