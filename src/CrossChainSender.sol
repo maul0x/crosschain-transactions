e6567e8dce58df5393f4462beb654d25f2ac0b2f
maul0x <501staudits@gmail.com>
2025-10-01 07:12:46 -0700

    feat: Add wormhole-solidity-sdk Library Imports

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
+import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
 contract CrossChainSender {}
