745ad87e99c8ac42617c20ad38a41511bb5ba013
maul0x <501staudits@gmail.com>
2025-10-01 07:13:52 -0700

    feat: Add Contract TokenSender Inheritance

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
 import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
-contract CrossChainSender {
+contract CrossChainSender is TokenSender {
 
         constructor(
         address _wormholeRelayer,
