fc33ac0fafc97006448cb3dfd1452ca10c526443
maul0x <501staudits@gmail.com>
2025-10-01 07:14:28 -0700

    feat: Add constant GAS_LIMIT Variable

--- src/CrossChainSender.sol
+++ src/CrossChainSender.sol
@@ -5,6 +5,7 @@ import "lib/wormhole-solidity-sdk/src/WormholeRelayerSDK.sol";
 import "lib/wormhole-solidity-sdk/src/interfaces/IERC20.sol";
 
 contract CrossChainSender is TokenSender {
+    uint256 constant GAS_LIMIT = 250_000;
 
         constructor(
         address _wormholeRelayer,
